ef4938bb85c101329d36a5081c4b84c413f13f06
tudda <tentufancr7@gmail.com>
2022-06-21 22:31:08 +0700

    testanimation

--- js/main.js
+++ js/main.js
@@ -77,3 +77,68 @@ checkbox.addEventListener("change", () => {
 
   document.getElementById("info").style.color = random_color;
 });
+//animation
+
+const observer = new IntersectionObserver(function (entries) {
+  entries.forEach(
+    (entry) => {
+      if (entry.intersectionRatio === 0.7) {
+        entry.target.classList.add("animationRight");
+      } else {
+        entry.target.classList.remove("animationRight");
+      }
+    },
+    {
+      threshold: 0.7,
+    },
+  );
+});
+
+const targets = document.querySelector(".img_animation");
+
+observer.observe(targets);
+
+//
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio === 0.5) {
+          console.log("test");
+          entry.target.classList.add("animationRight");
+          entry.target.classList.remove(".img_animation");
+        } else {
+          entry.target.classList.add("animationRight");
+          entry.target.classList.remove(".img_animation");
+        }
+      });
+    },
+    {
+      threshold: 0.5,
+    },
+  );
+
+  const elements = document.querySelectorAll(".img_animation");
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
+
+// const leftf = function (entries) {
+//   entries.forEach(
+//     (entry) => {
+//       entry.target.classList.toggle("animationLeft", entry.isIntersecting);
+//     },
+//     {
+//       threshold: 0.7,
+//     },
+//   );
+// };
+
+// const observer1 = new IntersectionObserver(leftf);
+
+// const lefts = document.querySelectorAll(".banner__heading ");
+// lefts.forEach((left) => {
+//   observer.observe(left);
+// });
